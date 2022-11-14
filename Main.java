d39381e6d76bc44a38ab304f7ce8884d7c5dcc54
suruthiganesh <111679759+suruthiganesh@users.noreply.github.com>
2022-11-14 15:27:03 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Stackexamples.com.gl;
+
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws CloneNotSupportedException
+	{
+		BuildingConstruction myDriverObj = new BuildingConstruction();
+
+		myDriverObj.getInputs();
+		myDriverObj.SortpushStack();
+		myDriverObj.DetremineBigfloorDay();
+		myDriverObj.checkSearchStack();
+		}
+
+}
